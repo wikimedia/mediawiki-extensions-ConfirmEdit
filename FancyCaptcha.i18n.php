964de363c0804115f0178e8dad2208b024876ce2
Leon Weber <leon@users.mediawiki.org>
2007-01-04 17:44:01 +0000

    Whoops, forgot to commit the third patch raymond sent me :))

--- FancyCaptcha.i18n.php
+++ FancyCaptcha.i18n.php
@@ -18,6 +18,17 @@ spam, please enter the words that appear below in the box ([[Special:Captcha/hel
 that appear below in the box ([[Special:Captcha/help|more info]]):',
 ),
 
+/* German */
+'de' => array(
+'fancycaptcha-edit' => 'Ihre Bearbeitung enthält neue externe Links. .
+Zum Schutz vor automatisiertem Spamming ist es nötig, dass Sie das nachfolgende Wort
+in das darunter erscheinende Feld eintragen. Klicken Sie dann erneut auf „Seite speichern“.
+<br />[[{{ns:special}}:Captcha/help|(Was soll das?)]]',
+'fancycaptcha-createaccount' => 'Zum Schutz vor automatisierter Anlage von Benutzerkonten
+ist es nötig, dass Sie das nachfolgende Wort in das darunter erscheinende Feld eintragen.
+[[{{ns:special}}:Captcha/help|(Fragen oder Probleme?)]]',
+),
+
 	);
 }
 

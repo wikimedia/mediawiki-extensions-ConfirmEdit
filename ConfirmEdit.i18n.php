838a6923b46a7032b2e79f13ff1ce1aed23d8f31
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-21 18:40:54 +0000

    Localisation updates from Betawiki. * Fixes and additions to 13 extensions for ca, dsb, it, tet.

--- ConfirmEdit.i18n.php
+++ ConfirmEdit.i18n.php
@@ -341,23 +341,24 @@ Notaðu „back“-hnapp vafrans til að halda áfram.",
 	'captcha-createaccount-fail' => "Staðfestingarkóðinn var rangur eða ekki til staðar.",
 );
 $wgConfirmEditMessages['it'] = array(
-	'captcha-edit'               => "La modifica richiesta aggiunge dei nuovi collegamenti (URL) alla pagina; come misura precauzionale contro l'inserimento automatico di spam, per confermarla è necessario inserire le parole che appaiono nell'immagine:<br />
-([[Special:Captcha/help|Cosa vuol dire?]])",
-	'captcha-addurl'               => "La modifica richiesta aggiunge dei nuovi collegamenti (URL) alla pagina; come misura precauzionale contro l'inserimento automatico di spam, per confermarla è necessario inserire le parole che appaiono nell'immagine:<br />
-([[Special:Captcha/help|Cosa vuol dire?]])",
-	'captcha-create'               => "La modifica richiesta aggiunge dei nuovi collegamenti (URL) alla pagina; come misura precauzionale contro l'inserimento automatico di spam, per confermarla è necessario inserire le parole che appaiono nell'immagine:<br />
-([[Special:Captcha/help|Cosa vuol dire?]])",
-	'captchahelp-title'          => "Cos'è il captcha?",
-	'captchahelp-text'           => "Capita spesso che i siti Web che accettano messaggi pubblici, come questo wiki, siano presi di mira da spammer che usano strumenti automatici per inserire collegamenti pubblicitari verso un gran numero di siti. Per quanto i collegamenti indesiderati si possano rimuovere, si tratta comunque di una seccatura non indifferente.
+	'captcha-edit'                => 'La modifica richiesta aggiunge dei nuovi collegamenti (URL) alla pagina; come misura precauzionale contro l\'inserimento automatico di spam, per confermarla è necessario inserire le parole che appaiono nell\'immagine:<br />
+([[Special:Captcha/help|Cosa vuol dire?]])',
+	'captcha-addurl'              => 'La modifica richiesta aggiunge dei nuovi collegamenti (URL) alla pagina; come misura precauzionale contro l\'inserimento automatico di spam, per confermarla è necessario inserire le parole che appaiono nell\'immagine:<br />
+([[Special:Captcha/help|Cosa vuol dire?]])',
+	'captcha-badpass'             => 'Ai fini di contrastare tentativi di forzatura automatica della password, risolvi per favore il seguente semplice calcolo ed inserisci la risposta nella casella ([[Speciale:Captcha/help|maggiori informazioni]]):',
+	'captcha-createaccount'       => 'Come misura di sicurezza nei confronti dei messaggi di spam automatici, per registrarsi è necessario inserire le parole che appaiono nell\'immagine:<br />
+([[Speciale:Captcha/help|Cosa vuol dire?]])',
+	'captcha-createaccount-fail'  => 'Codice di verifica errato o mancante.',
+	'captcha-create'              => 'La modifica richiesta aggiunge dei nuovi collegamenti (URL) alla pagina; come misura precauzionale contro l\'inserimento automatico di spam, per confermarla è necessario inserire le parole che appaiono nell\'immagine:<br />
+([[Special:Captcha/help|Cosa vuol dire?]])',
+	'captchahelp-title'           => 'Cos\'è il captcha?',
+	'captchahelp-text'            => 'Capita spesso che i siti Web che accettano messaggi pubblici, come questo wiki, siano presi di mira da spammer che usano strumenti automatici per inserire collegamenti pubblicitari verso un gran numero di siti. Per quanto i collegamenti indesiderati si possano rimuovere, si tratta comunque di una seccatura non indifferente.
 
-In alcuni casi, ad esempio quando si tenta di aggiungere nuovi collegamenti Web in una pagina, il software wiki può mostrare una immagine con un breve testo colorato e/o distorto chiedendo di riscriverlo in un'apposita finestrella. Poiché si tratta di un'azione difficile da replicare da parte di un computer, questo meccanismo consente a (quasi tutti) gli utenti reali di completare l'inserimento desiderato, impedendo l'accesso alla maggior parte degli spammer e degli altri attacchi automatizzati.
+In alcuni casi, ad esempio quando si tenta di aggiungere nuovi collegamenti Web in una pagina, il software wiki può mostrare una immagine con un breve testo colorato e/o distorto chiedendo di riscriverlo in un\'apposita finestrella. Poiché si tratta di un\'azione difficile da replicare da parte di un computer, questo meccanismo consente a (quasi tutti) gli utenti reali di completare l\'inserimento desiderato, impedendo l\'accesso alla maggior parte degli spammer e degli altri attacchi automatizzati.
 
-Sfortunatamente, queste misure di sicurezza possono mettere in difficoltà gli utenti con problemi visivi o coloro che utilizzano browser testuali o basati sulla sintesi vocale. Purtroppo al momento non è disponibile un meccanismo alternativo basato su messaggi audio; se queste procedure impediscono l'inserimento informazioni che si ritengono legittime, si prega di contattare gli amministratori del sito e chiedere loro assistenza.
+Sfortunatamente, queste misure di sicurezza possono mettere in difficoltà gli utenti con problemi visivi o coloro che utilizzano browser testuali o basati sulla sintesi vocale. Purtroppo al momento non è disponibile un meccanismo alternativo basato su messaggi audio; se queste procedure impediscono l\'inserimento informazioni che si ritengono legittime, si prega di contattare gli amministratori del sito e chiedere loro assistenza.
 
-Fare clic sul pulsante 'back' del browser per tornare alla pagina di modifica.",
-	'captcha-createaccount'      => "Come misura di sicurezza nei confronti dei messaggi di spam automatici, per registrarsi è necessario inserire le parole che appaiono nell'immagine:<br />
-([[Speciale:Captcha/help|Cosa vuol dire?]])",
-	'captcha-createaccount-fail' => "Codice di verifica errato o mancante.",
+Fare clic sul pulsante \'back\' del browser per tornare alla pagina di modifica.',
 );
 
 $wgConfirmEditMessages['ja'] = array(

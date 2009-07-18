73cfd41936d8d494b502dbd51ab5a864a9445c80
Alex Z. <mrzman@users.mediawiki.org>
2009-07-18 21:47:20 +0000

    The EditPage object is not passed by reference in the EditFilter hooks

--- ConfirmEdit_body.php
+++ ConfirmEdit_body.php
@@ -15,11 +15,11 @@ class ConfirmEditHooks {
 		return $wgCaptcha;
 	}
 
-	static function confirmEdit( &$editPage, $newtext, $section ) {
+	static function confirmEdit( $editPage, $newtext, $section ) {
 		return self::getInstance()->confirmEdit( $editPage, $newtext, $section );
 	}
 
-	static function confirmEditMerged( &$editPage, $newtext ) {
+	static function confirmEditMerged( $editPage, $newtext ) {
 		return self::getInstance()->confirmEditMerged( $editPage, $newtext );
 	}
 	
@@ -467,7 +467,7 @@ class SimpleCaptcha {
 	 * Backend function for confirmEdit() and confirmEditAPI()
 	 * @return bool false if the CAPTCHA is rejected, true otherwise
 	 */
-	private function doConfirmEdit( &$editPage, $newtext, $section, $merged = false ) {
+	private function doConfirmEdit( $editPage, $newtext, $section, $merged = false ) {
 		if( $this->shouldCheck( $editPage, $newtext, $section, $merged ) ) {
 			if( $this->passCaptcha() ) {
 				return true;
@@ -488,7 +488,7 @@ class SimpleCaptcha {
 	 * @param bool $merged
 	 * @return bool true to continue saving, false to abort and show a captcha form
 	 */
-	function confirmEdit( &$editPage, $newtext, $section, $merged = false ) {
+	function confirmEdit( $editPage, $newtext, $section, $merged = false ) {
 		if( defined('MW_API') ) {
 			# API mode
 			# The CAPTCHA was already checked and approved 
@@ -506,7 +506,7 @@ class SimpleCaptcha {
 	 * @param EditPage $editPage
 	 * @param string $newtext
 	 */
-	function confirmEditMerged( &$editPage, $newtext ) {
+	function confirmEditMerged( $editPage, $newtext ) {
 		return $this->confirmEdit( $editPage, $newtext, false, true );
 	}
 	

4776cfc3d4f7ad0397791c3bf10f5d488034ceee
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-07-19 21:22:03 +0000

    Localisation updates for extension messages from translatewiki.net (2009-07-19 21:09 UTC)

--- QuestyCaptcha.i18n.php
+++ QuestyCaptcha.i18n.php
@@ -26,3 +26,76 @@ Please contact the [[{{MediaWiki:Grouppage-sysop}}|site administrators]] for ass
 
 Click the 'back' button in your browser to return to the page editor.",
 );
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'questycaptcha-addurl' => 'In Dynere Bearbeitig het s neji extärni Gleicher (Links).
+As Schutz gege automatischi Spam, beantwort bitte d FRog, wu do unter gnännt wird ([[Special:Captcha/help|meh Informatione]]):',
+	'questycaptcha-badlogin' => 'As Schutz gege ne automatisch Passwort-Knacke, beantwort bitte d Frog, wu do unte gnännt wird ([[Special:Captcha/help|meh Informatione]]):',
+	'questycaptcha-createaccount' => 'As Schutz gege ne automatisch Aalege vu Benutzerkonte, beantwort bitte d Frog, wu do unte gnännt wird ([[Special:Captcha/help|meh Informatione]]):',
+	'questycaptcha-create' => 'Go d Syte aalege, beantwort bitte d Frog, wu do unte gnännt wird ([[Special:Captcha/help|meh Informatione]]):',
+	'questycaptcha-edit' => 'Go die Syte bearbeite, beantwort bitte d Frog, wu do unte gnännt wird ([[Special:Captcha/help|meh Informatione]]):',
+	'questycaptchahelp-text' => "Websyte, wu jede cha byytrage, wie des Wiki, wäre vylmol missbrucht vu Spammer, wu automatischi Wärchzyg bruche go ihri Gleicher (Links) in meglischt vyli Syte yyfiege.
+Au wänn die Spamgleicher chenne wider uusegnuu wäre, sin si einewäg e zimlig Ärgernis.
+
+Ab un zue, vor allem wänn neji Netzgleicher (Weblinks) in e Syte yygfiegt wäre, forderet s Wiki Di villicht uf, e Frog z beantworte.
+Wel des e Ufgab isch, wu mer schwär cha automatisiere, isch des e Megligkeit, Spammer un anderi automatischi Attacke z verhindere, derwylscht di meischte mänschlige Benutzer ihri Bearbeitige chenne byytrage.
+
+Bitte nimm Kontakt uf zue dr [[{{MediaWiki:Grouppage-sysop}}|Website-Administratore]] fir Hilf, wänn des unerwarteterwys verhinderet, ass Du chasch rächtmäßigi Bearbeitige mache.
+
+Druck dr 'Zruck'-Chnopf in Dyym Browser go zuem Bearbeitigsfänschter zruckgoh.",
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'questycaptcha-addurl' => 'Ding Änderung säz neu Lengks noh ußerhallef vum Wiki.
+Öm uns jäje der automattesch dobeijedonn <i lang="en">SPAM</i> ze hellefe,
+bes esu joot un donn di Frooch be_anntwoode, di heh dronger shteiht.
+([[Special:Captcha/help|Mieh Enfommazjuhne]])',
+	'questycaptcha-badlogin' => 'Öm uns jäje et automattesche Paßwootknacke ze hellefe,
+bes esu joot un donn di Frooch be_anntwoode, di heh dronge shteiht.
+([[Special:Captcha/help|Mieh Enfommazjuhne]])',
+	'questycaptcha-createaccount' => 'Öm uns jäje et maßesch automattesch neu Metmaacher Aanlääje ze hellefe,
+bes esu joot un donn di Frooch be_anntwoode, di heh dronger shteiht.
+([[Special:Captcha/help|Mieh Enfommazjuhne]])',
+	'questycaptcha-create' => 'Öm di Sigg aanzelääje,
+bes esu joot, donn di Frooch be_anntwoode, di heh dronge shteiht.
+([[Special:Captcha/help|Mieh Enfommazjuhne]])',
+	'questycaptcha-edit' => 'Öm di Sigg ze änderee,
+bes esu joot, donn di Frooch be_anntwoode, di heh dronge shteiht.
+([[Special:Captcha/help|Mieh Enfommazjuhne]])',
+	'questycaptchahelp-text' => 'Websigge, di Beijdrääsch vun de Öffentleschkeit aannämme, wi dat Wiki heh,
+wääde öff vun <i lang="en">SPAM</i>mer heimjesöhk. Di bruche Projramme
+för ier Lengks udder annder Jedrieße automattesch en dousende Wikis erin
+ze bränge. Der <i lang="en">SPAM</i> kam_mer wider fott maache, ävver dä
+blief e Ärjeneß.
+
+Manschmohl, besönders, wann De neu Lengks en en Sigg donn wells, künnt et
+Wiki Desch bedde, en Frooch ze be_antwoode. Nohdämm dat schwiiresch mem
+automattesche Projramm henzekrijje es, löht et de Minsche ier Beidrääsch
+maache, deiht ävver de miehßte <i lang="en">SPAM</i>mer un ander Robots affhallde.
+
+Donn Desh aan de [[{{MediaWiki:Grouppage-sysop}}|Köbeße vum Wiki]] wende,
+wann et Der trozdämm en de Fööß kütt, un De Dinge aanshtändejje Beijdraach
+nit en et Wiki kriß!
+
+Dä „Retuur“-Lengk udder -Knopp vun Dingem Brauser brängk Desch wider op
+di Sigg zom Ändere, woh De jraad wohß.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'questycaptcha-addurl' => "An Ärer Ännerung sinn nei extern Linken.
+Fir d'Protectioun géint automatesche Spam ze vergréisseren, beäntwert w.e.g. déi Fro hei ënnedrënner ([[Special:Captcha/help|méi Informatiounen]]):",
+	'questycaptcha-badlogin' => "Fir d'Protectioun géint d'automatiséiert Knacke vu Passwierder ze vergréisseren, beäntwert w.e.g. déi Fro hei ënnedrënner ([[Special:Captcha/help|méi Informatiounen]]):",
+	'questycaptcha-createaccount' => "Fir d'Protectioun géint d'automatiséiert Uleeë vu Benotzerkonten ze vergréisseren, beäntwert w.e.g. déi Fro hei ënnedrënner ([[Special:Captcha/help|méi Informatiounen]]):",
+	'questycaptcha-create' => "Fir d'Säit unzeleeën, beäntwert w.e.g. déi Fro hei ënnedrënner ([[Special:Captcha/help|méi Informatiounen]]):",
+	'questycaptcha-edit' => "Fir dës Säit z'änneren, beäntwert w.e.g. déi Fro hei ënnedrënner ([[Special:Captcha/help|méi Informatiounen]]):",
+);
+

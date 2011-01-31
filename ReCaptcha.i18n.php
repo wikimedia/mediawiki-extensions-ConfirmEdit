64987729b0a0957817d8b90bd8bb9554e59545f0
Raimond Spekking <raymond@users.mediawiki.org>
2011-01-31 14:27:35 +0000

    Add reCAPTCHA to Translatewiki

--- ReCaptcha.i18n.php
+++ ReCaptcha.i18n.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Internationalisation file for the ReCaptcha module of the ConfirmEdit
+ * Internationalisation file for the reCAPTCHA module of the ConfirmEdit
  * extension.
  *
  * @addtogroup Extensions
@@ -10,10 +10,10 @@ $messages = array();
 
 /* English */
 $messages['en'] = array(
+	'recaptcha-desc' => 'reCAPTCHA module for Confirm Edit',
         'recaptcha-edit' => 'To help protect against automated edit spam, please type the two words you see in the box below:', 
-        'recaptcha-addurl' => 'Your edit includes new external links. To help protect against automated
-spam, please type the two words you see in the box below:',
-    'recaptcha-badpass' => 'To help protect against automated password cracking, please type the two words you see in the box below:',
+        'recaptcha-addurl' => 'Your edit includes new external links. To help protect against automated spam, please type the two words you see in the box below:',
+	'recaptcha-badpass' => 'To help protect against automated password cracking, please type the two words you see in the box below:',
 	'recaptcha-createaccount' => 'To help protect against automated account creation, please type the two words you see in the box below:',
 	'recaptcha-createaccount-fail' => "Incorrect or missing reCAPTCHA answer.",
 	'recaptcha-create' => 'To help protect against automated page creation, please type the two words you see in the box below:', 

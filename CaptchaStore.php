e28655e1242703d665e8080a6831ca474fed40a2
Happy-melon <happy-melon@users.mediawiki.org>
2011-04-24 16:49:49 +0000

    Follow-up r86813: fix exceptions seen on TWN.  Ironically, I changed the CaptchaStore from an interface to an abstract class precisely to move into it the function which was throwing the exception...  :D

--- CaptchaStore.php
+++ CaptchaStore.php
@@ -42,7 +42,7 @@ abstract class CaptchaStore {
 	public final static function get() {
 		if( !self::$instance instanceof self ){
 			global $wgCaptchaStorageClass;
-			if( in_array( 'CaptchaStore', class_implements( $wgCaptchaStorageClass ) ) ) {
+			if( in_array( 'CaptchaStore', class_parents( $wgCaptchaStorageClass ) ) ) {
 				self::$instance = new $wgCaptchaStorageClass;
 			} else {
 				throw new MWException( "Invalid CaptchaStore class $wgCaptchaStorageClass" );

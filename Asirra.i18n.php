208ae34b0a9f1df5ccbcd5f172ffa68ed8981772
Niklas Laxström <nikerabbit@users.mediawiki.org>
2011-12-09 16:10:27 +0000

    Re-committing Asirra module after cleanup. Ping r91976

---
+++ Asirra.i18n.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Internationalisation file for the Asirra module of the ConfirmEdit
+ * extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/* English */
+$messages['en'] = array(
+	'asirra-desc' => 'Asirra module for ConfirmEdit',
+	'asirra-edit' => 'To help protect against automated edit spam, please select just the cat photos in the box below:', 
+	'asirra-addurl' => 'Your edit includes new external links. To help protect against automated spam, please select just the cat photos in the box below:',
+	'asirra-badpass' => 'To help protect against automated password cracking, please select just the cat photos in the box below:',
+	'asirra-createaccount' => 'To help protect against automated account creation, please select just the cat photos in the box below:',
+	'asirra-createaccount-fail' => "Please correctly identify the cats.",
+	'asirra-create' => 'To help protect against automated page creation, please select just the cat photos in the box below:', 
+	'asirra-nojs' => '\'\'\'Please enable JavaScript and resubmit the page.\'\'\'',
+	'asirra-failed' => 'Please identify all cat images',
+);
+
+
+/** German (Deutsch) */
+$messages['de'] = array(
+	'asirra-desc' => 'Asirra-Modul für ConfirmEdit',
+	'asirra-edit' => 'Zum Schutz vor automatisiertem Spam, wähle bitte nur die Katzen im untenstehenden Feld aus:',
+	'asirra-addurl' => 'Deine Bearbeitung enthält neue externe Links. Zum Schutz vor automatisiertem Spam wähle bitte nur die Katzen im untenstehenden Feld aus:',
+	'asirra-badpass' => 'Zum Schutz gegen automatisiertes Knacken von Passwörtern, wähle bitte nur die Katzen im untenstehenden Feld aus:',
+	'asirra-createaccount' => 'Zum Schutz gegen automatisierte Erstellung von Benutzerkonten wähle bitte nur die Katzen im untenstehenden Feld aus:',
+	'asirra-createaccount-fail' => 'Bitte wähle nur Katzen aus.',
+	'asirra-create' => 'Zum Schutz gegen automatisierte Erstellung von Seiten, wähle bitte nur die Katzen im untenstehenden Feld aus:',
+	'asirra-nojs' => '\'\'\'Bitte aktiviere JavaScript und sende die Seite noch einmal ab.\'\'\''
+);

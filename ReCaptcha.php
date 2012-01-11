e838dbca5002434b3d0a4915bcba2861000b4839
Sam Reed <reedy@users.mediawiki.org>
2012-01-11 23:24:21 +0000

    Load the Recaptcha class in a way that has some chance of even working

--- ReCaptcha.php
+++ ReCaptcha.php
@@ -17,7 +17,11 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 require_once dirname( __FILE__ ) . '/ConfirmEdit.php';
 $wgCaptchaClass = 'ReCaptcha';
 
-$wgExtensionMessagesFiles['ReCaptcha'] = dirname( __FILE__ ) . '/ReCaptcha.i18n.php';
+$dir = dirname( __FILE__ );
+
+$wgExtensionMessagesFiles['ReCaptcha'] = $dir . '/ReCaptcha.i18n.php';
+
+$wgAutoloadClasses['ReCaptcha']= $dir . '/Captcha.php';
 
 require_once( 'recaptchalib.php' );
 
@@ -55,94 +59,7 @@ function efReCaptcha() {
 
 	if ($wgReCaptchaPublicKey == '' || $wgReCaptchaPrivateKey == '') {
 		die ('You need to set $wgReCaptchaPrivateKey and $wgReCaptchaPublicKey in LocalSettings.php to ' .
-			"use the reCAPTCHA plugin. You can sign up for a key <a href='" .
-			htmlentities(recaptcha_get_signup_url ($wgServerName, "mediawiki")) . "'>here</a>.");
-	}
-}
-
-
-class ReCaptcha extends SimpleCaptcha {
-
-	//reCAPTHCA error code returned from recaptcha_check_answer
-	private $recaptcha_error = null;
-
-	/**
-	 * Displays the reCAPTCHA widget.
-	 * If $this->recaptcha_error is set, it will display an error in the widget.
-	 *
-	 */
-	function getForm() {
-		global $wgReCaptchaPublicKey, $wgReCaptchaTheme;
-		$useHttps = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' );
-		$js = 'var RecaptchaOptions = ' . Xml::encodeJsVar( array( 'theme' => $wgReCaptchaTheme, 'tabindex' => 1  ) );
-
-		return Html::inlineScript( $js ) . recaptcha_get_html($wgReCaptchaPublicKey, $this->recaptcha_error, $useHttps);
-	}
-
-	/**
-	 * Calls the library function recaptcha_check_answer to verify the users input.
-	 * Sets $this->recaptcha_error if the user is incorrect.
-	 * @return boolean
-	 *
-	 */
-	function passCaptcha() {
-		global $wgReCaptchaPrivateKey, $wgRequest;
-
-		//API is hardwired to return wpCaptchaId and wpCaptchaWord, so use that if the standard two are empty
-		$challenge = $wgRequest->getVal( 'recaptcha_challenge_field', $wgRequest->getVal( 'wpCaptchaId' ) );
-		$response = $wgRequest->getVal( 'recaptcha_response_field', $wgRequest->getVal( 'wpCaptchaWord' ) );
-		
-		if ( $response === null ) {
-			//new captcha session
-			return false;
-		}
-
-		$recaptcha_response = recaptcha_check_answer(
-			$wgReCaptchaPrivateKey,
-			$wgRequest->getIP(),
-			$challenge,
-			$response
-		);
-		
-		if (!$recaptcha_response->is_valid) {
-			$this->recaptcha_error = $recaptcha_response->error;
-			return false;
-		}
-		
-		$recaptcha_error = null;
-		
-		return true;
-
-	}
-
-	function addCaptchaAPI( &$resultArr ) {
-		global $wgReCaptchaPublicKey;
-		$resultArr['captcha']['type'] = 'recaptcha';
-		$resultArr['captcha']['mime'] = 'image/png';
-		$resultArr['captcha']['key'] = $wgReCaptchaPublicKey;
-		$resultArr['captcha']['error'] = $this->recaptcha_error;
-	}
-
-	/**
-	 * Show a message asking the user to enter a captcha on edit
-	 * The result will be treated as wiki text
-	 *
-	 * @param $action Action being performed
-	 * @return string
-	 */
-	function getMessage( $action ) {
-		$name = 'recaptcha-' . $action;
-		$text = wfMsg( $name );
-		# Obtain a more tailored message, if possible, otherwise, fall back to
-		# the default for edits
-		return wfEmptyMsg( $name, $text ) ? wfMsg( 'recaptcha-edit' ) : $text;
-	}
-
-	public function APIGetAllowedParams( &$module, &$params ) {
-		return true;
-	}
-
-	public function APIGetParamDescription( &$module, &$desc ) {
-		return true;
+				"use the reCAPTCHA plugin. You can sign up for a key <a href='" .
+				htmlentities(recaptcha_get_signup_url ($wgServerName, "mediawiki")) . "'>here</a>.");
 	}
 }

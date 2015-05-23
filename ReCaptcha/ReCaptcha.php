023f24aa8a34841bfe71b1f594f10f58544933c3
Florian <florian.schmidt.welzow@t-online.de>
2015-05-23 11:32:16 +0200

    Clear PHP entry point and use extension.json for ReCaptcha
    Bug: T88047
    Change-Id: Id09113674c2a4c4cfd7539b3e752bfca776a5903
    (cherry picked from commit d7e46583ed293bcc6ac18fce30028536935ffb32)

--- ReCaptcha/ReCaptcha.php
+++ ReCaptcha/ReCaptcha.php
@@ -1,63 +1,13 @@
 <?php
-
-/**
- * Captcha class using the reCAPTCHA widget.
- * Stop Spam. Read Books.
- *
- * @addtogroup Extensions
- * @author Mike Crawford <mike.crawford@gmail.com>
- * @copyright Copyright (c) 2007 reCAPTCHA -- http://recaptcha.net
- * @licence MIT/X11
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	exit;
-}
-
-require_once dirname( __DIR__ ) . '/ConfirmEdit.php';
-$wgCaptchaClass = 'ReCaptcha';
-
-$wgMessagesDirs['ReCaptcha'] = __DIR__ . '/i18n';
-
-$wgAutoloadClasses['ReCaptcha'] = __DIR__ . '/ReCaptcha.class.php';
-
-require_once( 'recaptchalib.php' );
-
-// Set these in LocalSettings.php
-$wgReCaptchaPublicKey = '';
-$wgReCaptchaPrivateKey = '';
-// For backwards compatibility
-$recaptcha_public_key = '';
-$recaptcha_private_key = '';
-
-/**
- * Sets the theme for ReCaptcha
- *
- * See http://code.google.com/apis/recaptcha/docs/customization.html
- */
-$wgReCaptchaTheme = 'red';
-
-$wgExtensionFunctions[] = 'efReCaptcha';
-
-/**
- * Make sure the keys are defined.
- */
-function efReCaptcha() {
-	global $wgReCaptchaPublicKey, $wgReCaptchaPrivateKey;
-	global $recaptcha_public_key, $recaptcha_private_key;
-	global $wgServerName;
-
-	// Backwards compatibility
-	if ( $wgReCaptchaPublicKey == '' ) {
-		$wgReCaptchaPublicKey = $recaptcha_public_key;
-	}
-	if ( $wgReCaptchaPrivateKey == '' ) {
-		$wgReCaptchaPrivateKey = $recaptcha_private_key;
-	}
-
-	if ( $wgReCaptchaPublicKey == '' || $wgReCaptchaPrivateKey == '' ) {
-		die ( 'You need to set $wgReCaptchaPrivateKey and $wgReCaptchaPublicKey in LocalSettings.php to ' .
-				"use the reCAPTCHA plugin. You can sign up for a key <a href='" .
-				htmlentities( recaptcha_get_signup_url ( $wgServerName, "mediawiki" ) ) . "'>here</a>." );
-	}
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'ConfirmEdit/ReCaptcha' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['ReCaptcha'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for ReCaptcha extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the Re^Captcha extension requires MediaWiki 1.25+' );
 }

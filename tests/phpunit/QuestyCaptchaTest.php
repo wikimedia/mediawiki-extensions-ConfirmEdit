79b09f499515e96ab3575f147540aef310418379
Umherirrender <umherirrender_de.wp@web.de>
2017-12-27 21:59:19 +0100

    Register QuestyCaptcha class ealier in unit tests
    The captchas in production are registered conditional, in tests the
    class needs to be added to the autoloader.
    Moved this register up to setUp to detect the existing @covers correctly
    by the new testValidCovers from
    I1d564bcae2bfbedb004c440b90db6341148ed4ba
    
    Change-Id: I79f225126d7bbbfd436ab165bb98a0ed9517b8bc

--- tests/phpunit/QuestyCaptchaTest.php
+++ tests/phpunit/QuestyCaptchaTest.php
@@ -1,6 +1,16 @@
 <?php
 
 class QuestyCaptchaTest extends MediaWikiTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->mergeMwGlobalArrayValue(
+			'wgAutoloadClasses',
+			[ 'QuestyCaptcha' => __DIR__ . '/../../QuestyCaptcha/QuestyCaptcha.class.php' ]
+		);
+	}
+
 	/**
 	 * @covers QuestyCaptcha::getCaptcha
 	 * @dataProvider provideGetCaptcha
@@ -11,10 +21,6 @@ class QuestyCaptchaTest extends MediaWikiTestCase {
 			$GLOBALS['wgCaptchaQuestions'] = [];
 		}
 		$this->setMwGlobals( 'wgCaptchaQuestions', $config );
-		$this->mergeMwGlobalArrayValue(
-			'wgAutoloadClasses',
-			[ 'QuestyCaptcha' => __DIR__ . '/../../QuestyCaptcha/QuestyCaptcha.class.php' ]
-		);
 
 		$qc = new QuestyCaptcha();
 		$this->assertEquals( $expected, $qc->getCaptcha() );

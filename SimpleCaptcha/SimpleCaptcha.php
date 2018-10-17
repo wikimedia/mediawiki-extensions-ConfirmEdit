d22d1e88f47ac567dcb2f4a643f7afaeb43ae0a9
C. Scott Ananian <cscott@cscott.net>
2018-10-17 12:46:25 -0400

    Replace deprecated untidy OutputPage::addWikiText() method
    The replacement OutputPage::addWikiMsg() method is ancient, and so
    no bump to the minimum required MW version is needed.
    
    Bug: T198214
    Change-Id: I082bfb4585632dc37464d04aa93938ca05a9fdd0

--- SimpleCaptcha/SimpleCaptcha.php
+++ SimpleCaptcha/SimpleCaptcha.php
@@ -198,7 +198,7 @@ class SimpleCaptcha {
 
 		if ( $this->action !== 'edit' ) {
 			unset( $page->ConfirmEdit_ActivateCaptcha );
-			$out->addWikiText( $this->getMessage( $this->action )->plain() );
+			$out->addWikiMsg( $this->getMessage( $this->action )->getKey() );
 			$this->addFormToOutput( $out );
 		}
 	}
@@ -214,7 +214,7 @@ class SimpleCaptcha {
 		if ( isset( $page->ConfirmEdit_ActivateCaptcha ) ||
 			$this->shouldCheck( $page, '', '', $context )
 		) {
-			$out->addWikiText( $this->getMessage( $this->action )->plain() );
+			$out->addWikiMsg( $this->getMessage( $this->action )->getKey() );
 			$this->addFormToOutput( $out );
 		}
 		unset( $page->ConfirmEdit_ActivateCaptcha );

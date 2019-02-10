28cb4a71da22d49045cd174c82cfdace9de04b6b
Ed Sanders <esanders@wikimedia.org>
2019-02-10 15:44:37 +0000

    build: Update eslint-config-wikimedia to 0.10.1
    Change-Id: I78b2a1c233c97af39e1c8f8fd9d839d0ffc4936e

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,9 @@ module.exports = function ( grunt ) {
 
 	grunt.initConfig( {
 		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true
+			},
 			all: [
 				'**/*.js',
 				'!node_modules/**',

41fbcb2963735ac11e607dc88423d5a39f2db8d6
Kosta Harlan <kharlan@wikimedia.org>
2024-05-05 10:50:21 +0300

    phan: Load AbuseFilter in phan config
    See I76e8ce9c1533ca8ad47abb1ffd2e683cc85b78aa for CI config change
    
    Change-Id: Iecea4af54b972637fb2c4c2ce234708f8eb4e4b9

--- .phan/config.php
+++ .phan/config.php
@@ -10,6 +10,14 @@ $cfg['directory_list'] = array_merge(
 		'QuestyCaptcha/',
 		'ReCaptchaNoCaptcha/',
 		'SimpleCaptcha/',
+		'../../extensions/AbuseFilter',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/AbuseFilter',
 	]
 );
 

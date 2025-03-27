b81b4fcb0a7ed31f253af63c5572b234767b1130
Reedy <reedy@wikimedia.org>
2025-03-27 17:48:24 +0000

    CaptchaPreAuthenticationProvider: Minor cleanup
    Change-Id: I5943c52a173c2b8a05f53c6361dbb54ae328ad2f

--- includes/Auth/CaptchaPreAuthenticationProvider.php
+++ includes/Auth/CaptchaPreAuthenticationProvider.php
@@ -19,6 +19,8 @@ class CaptchaPreAuthenticationProvider extends AbstractPreAuthenticationProvider
 		$captcha = Hooks::getInstance();
 		$user = User::newFromName( $options['username'] );
 
+		$logger = LoggerFactory::getInstance( 'captcha' );
+
 		$needed = false;
 		switch ( $action ) {
 			case AuthManager::ACTION_CREATE:
@@ -26,29 +28,28 @@ class CaptchaPreAuthenticationProvider extends AbstractPreAuthenticationProvider
 				$needed = $captcha->needCreateAccountCaptcha( $u );
 				if ( $needed ) {
 					$captcha->setAction( 'accountcreate' );
-					// This is debug level simply because generally
-					// captchas are either always or never triggered on
-					// view of create account, so it gets pretty noisy
-					LoggerFactory::getInstance( 'captcha' )
-						->debug( 'Captcha shown on account creation for {user}', [
-							'event' => 'captcha.display',
-							'eventType' => 'accountcreation',
-							'user' => $u->getName()
-						] );
+					// This is debug level simply because generally a
+					// captcha is either always or never triggered on
+					// view of Special:CreateAccount, so it gets pretty noisy
+					$logger->debug( 'Captcha shown on account creation for {user}', [
+						'event' => 'captcha.display',
+						'eventType' => 'accountcreation',
+						'user' => $u->getName()
+					] );
 				}
 				break;
 			case AuthManager::ACTION_LOGIN:
 				// Captcha is shown on login when there were too many failed attempts from the current IP
 				// or using a given username, or if a hook handler says that a CAPTCHA should be shown.
 				// The varying on username is a bit awkward because we don't know the
-				// username yet. The username from the last successful login is stored in a cookie,
-				// but we still must make sure to not lock out other usernames, so after the first
+				// username yet. The username from the last successful login is stored in a cookie.
+				// We still must make sure to not lock out other usernames, so after the first
 				// failed login attempt using a username that needs a captcha, set a session flag
 				// to display a captcha on login from that point on. This will result in confusing
 				// error messages if the browser cannot persist the session (because we'll never show
 				// a required captcha field), but then login would be impossible anyway, so no big deal.
 
-				// If the username ends to be one that does not trigger the captcha, that will
+				// If the username ends up to be one that does not trigger the captcha, that will
 				// result in weird behavior (if the user leaves the captcha field empty, they get
 				// a required field error; if they fill it with an invalid answer, it will pass)
 				// - again, not a huge deal.
@@ -56,14 +57,13 @@ class CaptchaPreAuthenticationProvider extends AbstractPreAuthenticationProvider
 				$suggestedUsername = $session->suggestLoginUsername();
 				if ( $captcha->triggersCaptcha( CaptchaTriggers::LOGIN_ATTEMPT ) ) {
 					$captcha->setAction( 'loginattempt' );
-					LoggerFactory::getInstance( 'captcha' )
-						->info( 'Captcha shown on login attempt by {clientip} for {suggestedUser}', [
-							'event' => 'captcha.display',
-							'eventType' => 'loginattempt',
-							'suggestedUser' => $suggestedUsername,
-							'clientip' => $this->manager->getRequest()->getIP(),
-							'ua' => $this->manager->getRequest()->getHeader( 'User-Agent' )
-						] );
+					$logger->info( 'Captcha shown on login attempt by {clientip} for {suggestedUser}', [
+						'event' => 'captcha.display',
+						'eventType' => 'loginattempt',
+						'suggestedUser' => $suggestedUsername,
+						'clientip' => $this->manager->getRequest()->getIP(),
+						'ua' => $this->manager->getRequest()->getHeader( 'User-Agent' )
+					] );
 					$needed = true;
 					break;
 				}
@@ -75,26 +75,21 @@ class CaptchaPreAuthenticationProvider extends AbstractPreAuthenticationProvider
 					|| $loginCounter->isBadLoginTriggered()
 					|| ( $suggestedUsername && $loginCounter->isBadLoginPerUserTriggered( $suggestedUsername ) )
 				) {
-					$needed = true;
 					$captcha->setAction( 'badlogin' );
-					LoggerFactory::getInstance( 'captcha' )
-						->info( 'Captcha shown on login by {clientip} for {suggestedUser}', [
-							'event' => 'captcha.display',
-							'eventType' => 'badlogin',
-							'suggestedUser' => $suggestedUsername,
-							'clientip' => $this->manager->getRequest()->getIP(),
-							'ua' => $this->manager->getRequest()->getHeader( 'User-Agent' )
-						] );
+					$logger->info( 'Captcha shown on login by {clientip} for {suggestedUser}', [
+						'event' => 'captcha.display',
+						'eventType' => 'badlogin',
+						'suggestedUser' => $suggestedUsername,
+						'clientip' => $this->manager->getRequest()->getIP(),
+						'ua' => $this->manager->getRequest()->getHeader( 'User-Agent' )
+					] );
+					$needed = true;
 					break;
 				}
 				break;
 		}
 
-		if ( $needed ) {
-			return [ $captcha->createAuthenticationRequest() ];
-		}
-
-		return [];
+		return $needed ? [ $captcha->createAuthenticationRequest() ] : [];
 	}
 
 	/** @inheritDoc */
@@ -120,14 +115,13 @@ class CaptchaPreAuthenticationProvider extends AbstractPreAuthenticationProvider
 				$captcha->setTrigger( "post-badlogin login '$username'" );
 			}
 			$success = $this->verifyCaptcha( $captcha, $reqs, new User() );
-			$ip = $this->manager->getRequest()->getIP();
 			$action = $loginTriggersCaptcha ? 'login page' : 'bad login';
 			LoggerFactory::getInstance( 'captcha' )->info( "Captcha shown on $action for {user}", [
 				'event' => 'captcha.submit',
 				'eventType' => $loginTriggersCaptcha ? 'loginattempt' : 'badlogin',
 				'successful' => $success,
 				'user' => $username ?? 'unknown',
-				'clientip' => $ip,
+				'clientip' => $this->manager->getRequest()->getIP(),
 				'ua' => $this->manager->getRequest()->getHeader( 'User-Agent' )
 			] );
 		}
@@ -177,8 +171,7 @@ class CaptchaPreAuthenticationProvider extends AbstractPreAuthenticationProvider
 		switch ( $response->status ) {
 			case AuthenticationResponse::PASS:
 			case AuthenticationResponse::RESTART:
-				$session = $this->manager->getRequest()->getSession();
-				$session->remove( 'ConfirmEdit:loginCaptchaPerUserTriggered' );
+				$this->manager->getRequest()->getSession()->remove( 'ConfirmEdit:loginCaptchaPerUserTriggered' );
 				$loginCounter->resetBadLoginCounter( $user ? $user->getName() : null );
 				break;
 			case AuthenticationResponse::FAIL:

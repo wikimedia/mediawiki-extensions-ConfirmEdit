5c301835497171daf3a7f992b18f4ec4a354af44
Reedy <reedy@wikimedia.org>
2025-05-28 17:33:39 +0100

    GenerateFancyCaptchas: Consistent trailing full stops on output
    Change-Id: I95df3e617a3cbfeecba1d111f41b47b5c91374f1

--- maintenance/GenerateFancyCaptchas.php
+++ maintenance/GenerateFancyCaptchas.php
@@ -148,7 +148,7 @@ class GenerateFancyCaptchas extends Maintenance {
 
 		$this->output(
 			sprintf(
-				"\nGeneration script for %d captchas ran in %.1f seconds\n",
+				"\nGeneration script for %d captchas ran in %.1f seconds.\n",
 				$countGen,
 				$captchaTime
 			)
@@ -219,7 +219,7 @@ class GenerateFancyCaptchas extends Maintenance {
 			$this->output( " Done.\n" );
 			$this->output(
 				sprintf(
-					"\nCopied %d captchas to storage in %.1f seconds\n",
+					"\nCopied %d captchas to storage in %.1f seconds.\n",
 					$ret->successCount,
 					$storeTime
 				)
@@ -263,7 +263,7 @@ class GenerateFancyCaptchas extends Maintenance {
 				$this->output( "Done.\n" );
 				$this->output(
 					sprintf(
-						"\nDeleted %d old captchas in %.1f seconds\n",
+						"\nDeleted %d old captchas in %.1f seconds.\n",
 						$numOriginalFiles,
 						$deleteTime
 					)
@@ -291,7 +291,7 @@ class GenerateFancyCaptchas extends Maintenance {
 		$totalTime += microtime( true );
 		$this->output(
 			sprintf(
-				"\nWhole captchas generation process took %.1f seconds\n",
+				"\nWhole captchas generation process took %.1f seconds.\n",
 				$totalTime
 			)
 		);

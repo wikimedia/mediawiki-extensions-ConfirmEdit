8c0dde24bc84471a5d299532cbe1dae396824dae
Máté Szabó <mszabo@wikimedia.org>
2025-06-23 17:11:56 +0200

    HTMLHCaptchaField: Add tests
    Why:
    
    - HTMLHCaptchaField currently does not have tests.
    
    What:
    
    - Add a test for this class.
    
    Bug: T394874
    Change-Id: If411ed37deed3718c8527230b4c9f53b3dcfcb1b

---
+++ tests/phpunit/HTMLHCaptchaFieldTest.php
@@ -0,0 +1,117 @@
+<?php
+namespace MediaWiki\Extension\ConfirmEdit\Test;
+
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\ConfirmEdit\hCaptcha\HTMLHCaptchaField;
+use MediaWiki\HTMLForm\HTMLForm;
+use MediaWiki\Output\OutputPage;
+use MediaWiki\Request\ContentSecurityPolicy;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @covers \MediaWiki\Extension\ConfirmEdit\hCaptcha\HTMLHCaptchaField
+ */
+class HTMLHCaptchaFieldTest extends MediaWikiIntegrationTestCase {
+	/**
+	 * @dataProvider provideOptions
+	 */
+	public function testRender(
+		array $configOverrides,
+		array $params,
+		string $expectedHtml
+	): void {
+		$this->overrideConfigValues( $configOverrides );
+
+		$defaultSrcs = [];
+		$scriptSrcs = [];
+		$styleSrcs = [];
+
+		$csp = $this->createMock( ContentSecurityPolicy::class );
+		$csp->method( 'addDefaultSrc' )
+			->willReturnCallback( static function ( $src ) use ( &$defaultSrcs ): void {
+				$defaultSrcs[] = $src;
+			} );
+		$csp->method( 'addScriptSrc' )
+			->willReturnCallback( static function ( $src ) use ( &$scriptSrcs ): void {
+				$scriptSrcs[] = $src;
+			} );
+		$csp->method( 'addStyleSrc' )
+			->willReturnCallback( static function ( $src ) use ( &$styleSrcs ): void {
+				$styleSrcs[] = $src;
+			} );
+
+		$output = $this->createMock( OutputPage::class );
+		$output->method( 'getCSP' )
+			->willReturn( $csp );
+		$output->expects( $this->once() )
+			->method( 'addHeadItem' )
+			->with(
+				'h-captcha',
+				"<script src=\"{$configOverrides['HCaptchaApiUrl']}\" async defer></script>"
+			);
+
+		$context = RequestContext::getMain();
+		$context->setLanguage( 'qqx' );
+		$context->setOutput( $output );
+
+		$form = HTMLForm::factory( 'ooui', [], $context );
+		$field = new HTMLHCaptchaField( $params + [ 'parent' => $form, 'name' => 'ignored' ] );
+
+		$this->assertSame( 'h-captcha-response', $field->getName() );
+		$this->assertSame( $expectedHtml, $field->getInputHTML( null ) );
+
+		$this->assertSame( $configOverrides['HCaptchaCSPRules'], $defaultSrcs );
+		$this->assertSame( $configOverrides['HCaptchaCSPRules'], $styleSrcs );
+		$this->assertSame( $configOverrides['HCaptchaCSPRules'], $scriptSrcs );
+	}
+
+	public static function provideOptions(): iterable {
+		$testApiUrl = 'https://hcaptcha.example.com/api';
+		$testSiteKey = 'foo';
+		$testCspRules = [
+			'https://hcaptcha.example.com',
+			'https://hcaptcha-2.example.com'
+		];
+
+		yield 'active mode, no prior error' => [
+			[
+				'HCaptchaApiUrl' => $testApiUrl,
+				'HCaptchaPassiveMode' => false,
+				'HCaptchaCSPRules' => $testCspRules,
+			],
+			[ 'key' => $testSiteKey ],
+			"<div class=\"h-captcha\" data-sitekey=\"$testSiteKey\"></div>"
+		];
+
+		yield 'passive mode, no prior error' => [
+			[
+				'HCaptchaApiUrl' => $testApiUrl,
+				'HCaptchaPassiveMode' => true,
+				'HCaptchaCSPRules' => $testCspRules,
+			],
+			[ 'key' => $testSiteKey ],
+			"<div class=\"h-captcha\" data-sitekey=\"$testSiteKey\"></div>(hcaptcha-privacy-policy)"
+		];
+
+		yield 'active mode, prior error set' => [
+			[
+				'HCaptchaApiUrl' => $testApiUrl,
+				'HCaptchaPassiveMode' => false,
+				'HCaptchaCSPRules' => $testCspRules,
+			],
+			[ 'key' => $testSiteKey, 'error' => 'some-error' ],
+			"<div class=\"h-captcha mw-confirmedit-captcha-fail\" data-sitekey=\"$testSiteKey\"></div>"
+		];
+
+		yield 'passive mode, prior error set' => [
+			[
+				'HCaptchaApiUrl' => $testApiUrl,
+				'HCaptchaPassiveMode' => true,
+				'HCaptchaCSPRules' => $testCspRules,
+			],
+			[ 'key' => $testSiteKey, 'error' => 'some-error' ],
+			"<div class=\"h-captcha mw-confirmedit-captcha-fail\" data-sitekey=\"$testSiteKey\"></div>" .
+			"(hcaptcha-privacy-policy)"
+		];
+	}
+}

1cbcec3e5de179a9586facd70afd6fd1c74ba083
Máté Szabó <mszabo@wikimedia.org>
2025-07-14 18:22:39 +0200

    Use hCaptcha onload parameter in SE mode
    Why:
    
    - In Secure Enclave mode, we load the hCaptcha SDK via
      mw.loader.getScript(), and run init code when the promise returned by
      this function resolves.
    - This happens too early, causing hCaptcha to be run with an
      inconsistent config.
    
    What:
    
    - Use the onload parameter in the SDK URL instead to specify a callback
      that will be invoked by the SDK at the right time.
    
    Change-Id: If2fa229125f544c88b8ae7443c683adbc01942e0

--- resources/ext.confirmEdit.hCaptcha/secureEnclave.js
+++ resources/ext.confirmEdit.hCaptcha/secureEnclave.js
@@ -2,21 +2,31 @@
 
 const config = require( './config.json' );
 
+/**
+ * Load hCaptcha in Secure Enclave mode.
+ */
 function useSecureEnclave() {
-	mw.loader.getScript( config.HCaptchaApiUrl )
-		.then( () => {
-			const captchaID = hcaptcha.render(
-				'h-captcha',
-				{
-					callback: function ( token ) {
-						// handle hCaptcha response token
-						// eslint-disable-next-line no-jquery/no-global-selector
-						$( '#h-captcha-response' ).val( token );
-					}
+	// NOTE: Use hCaptcha's onload parameter rather than the return value of getScript()
+	// to run init code, as the latter would run it too early and use a potentially inconsistent config.
+	window.onHCaptchaSDKLoaded = () => {
+		const captchaID = hcaptcha.render(
+			'h-captcha',
+			{
+				callback: function ( token ) {
+					// handle hCaptcha response token
+					// eslint-disable-next-line no-jquery/no-global-selector
+					$( '#h-captcha-response' ).val( token );
 				}
-			);
-			hcaptcha.execute( captchaID );
-		} )
+			}
+		);
+
+		hcaptcha.execute( captchaID );
+	};
+
+	const hCaptchaApiUrl = new URL( config.HCaptchaApiUrl );
+	hCaptchaApiUrl.searchParams.set( 'onload', 'onHCaptchaSDKLoaded' );
+
+	mw.loader.getScript( hCaptchaApiUrl.toString() )
 		.catch( () => {
 			mw.errorLogger.logError(
 				new Error( 'Unable to load hCaptcha script in secure enclave mode' ),

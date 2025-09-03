7f55833333be8161756f1be86127b2bb51fb56dd
Kosta Harlan <kharlan@wikimedia.org>
2025-09-03 09:18:26 +0200

    Hooks: Add missing action in onEditFilterMergedContent
    Why:
    
    - We need to specify that the action is `edit` so that per-action
      Captcha class configuration can function properly
    
    What:
    
    - Specify `CaptchaTriggers::EDIT` when obtaining SimpleCaptcha in the
      onEditFilterMergedContent hook
    
    Follows-Up: Idc47bdae8007da938f31e1c0f33e9be4813f41d7
    Change-Id: Idc160daf4506528d96c1e9101d74f4edbeebf58d
    (cherry picked from commit e6caa3a857bf0432e81db8f0a041cc99759fc874)

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -102,7 +102,7 @@ class Hooks implements
 	public function onEditFilterMergedContent( IContextSource $context, Content $content, Status $status,
 		$summary, User $user, $minorEdit
 	) {
-		$simpleCaptcha = self::getInstance();
+		$simpleCaptcha = self::getInstance( CaptchaTriggers::EDIT );
 		// Set a flag indicating that ConfirmEdit's implementation of
 		// EditFilterMergedContent ran. This can be checked by other extensions
 		// e.g. AbuseFilter.

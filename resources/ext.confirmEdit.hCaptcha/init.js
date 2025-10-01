43aea4107a858d5107b3a0a29702f906c774e23d
Dreamy Jazz <wpgbrown@wikimedia.org>
2025-10-01 20:51:55 +0100

    Merge ve-confirmedit-hCaptcha module into ext.confirmEdit.hCaptcha
    Why:
    * We want to reuse code from ext.confirmEdit.hCaptcha in the
      VisualEditor hCaptcha module (ve-confirmedit-hCaptcha)
    ** This is to reduce code duplication and allow us to fix the
       VisualEditor handling for when hCaptcha secure enclave mode
       is enabled
    * We cannot easily require files across modules, so we would either
      need to merge these modules or include the relevant JS files
      in the VisualEditor module
    ** Including the files in the VisualEditor module would require
       adding most of the JS files in ext.confirmEdit.hCaptcha to
       ve-confirmedit-hCaptcha, which will mean any new files may
       need to be added to two places in extension.json to avoid
       errors.
    ** Merging the modules has it's own complications, but we can
       avoid these using an approach that is used by GrowthExperiments
       to conditionally load code as a VisualEditor plugin if
       VisualEditor is loaded on the page
    
    What:
    * Move ve.init.mw.HCaptchaSaveErrorHandler.js to the
      ext.confirmEdit.hCaptcha module
    ** No changes, except from those which are necessary, have been
       made so that it's easier to verify that the functionality hasn't
       changed in this commit
    *** The code being run needed to be placed into a callback so that
        we can execute only when VisualEditor modules are present on
        the page and also after VisualEditor JS has loaded.
    * Update ext.confirmEdit.hCaptcha/init.js to register the hCaptcha
      VisualEditor plugin using an approach used in GrowthExperiments
    ** This is only loaded if the 'ext.visualEditor.targetLoader'
       module is present in the page (i.e. one of 'loading', 'loaded',
       'ready', or 'registered' as determined by mw.loader.getState)
    *** When VisualEditor is not present on the page, the
        mw.loader.getState method will return 'missing' and so the
        plugin will not be registered.
    * No tests exist for the VisualEditor plugin, but QUnit tests for
      this will be added in a future commit as part of T405585
    
    Bug: T405585
    Change-Id: If2e9db483e54487bce5dd30ec2af4ebad8982d16

--- resources/ext.confirmEdit.hCaptcha/init.js
+++ resources/ext.confirmEdit.hCaptcha/init.js
@@ -1,8 +1,21 @@
 $( () => {
 	const useSecureEnclave = require( './secureEnclave.js' );
 	const config = require( './config.json' );
+	const installHCaptchaVisualEditorPlugin = require( './ve/ve.init.mw.HCaptchaSaveErrorHandler.js' );
 
 	if ( config.HCaptchaEnterprise && config.HCaptchaSecureEnclave ) {
 		useSecureEnclave( window );
+	}
+
+	// If VisualEditor is available, then register the hCaptcha handler plugin.
+	// It may be loaded, loading, ready, or registered depending on when this
+	// module has been loaded. If it is 'missing' then we should not need to
+	// respond to any VisualEditor edit on this page.
+	const veState = mw.loader.getState( 'ext.visualEditor.targetLoader' );
+	const validStates = [ 'loading', 'loaded', 'ready', 'registered' ];
+	if ( validStates.includes( veState ) ) {
+		mw.loader.using( 'ext.visualEditor.targetLoader' ).then( () => {
+			mw.libs.ve.targetLoader.addPlugin( installHCaptchaVisualEditorPlugin );
+		} );
 	}
 } );
---
+++ resources/ext.confirmEdit.hCaptcha/ve/ve.init.mw.HCaptchaSaveErrorHandler.js
@@ -0,0 +1,69 @@
+const config = require( './../config.json' );
+
+/**
+ * Defines and installs the hCaptcha plugin for VisualEditor. This should be called
+ * only when VisualEditor is loaded and ideally through a callback provided to
+ * `mw.libs.ve.targetLoader.addPlugin`
+ */
+module.exports = () => {
+	ve.init.mw.HCaptchaSaveErrorHandler = function () {};
+
+	OO.inheritClass( ve.init.mw.HCaptchaSaveErrorHandler, ve.init.mw.SaveErrorHandler );
+
+	ve.init.mw.HCaptchaSaveErrorHandler.static.name = 'confirmEditHCaptcha';
+
+	ve.init.mw.HCaptchaSaveErrorHandler.static.getReadyPromise = function () {
+		if ( !this.readyPromise ) {
+			const deferred = $.Deferred();
+			const scriptURL = new URL( config.HCaptchaApiUrl, location.href );
+			const onLoadFn = 'onHcaptchaLoadCallback' + Date.now();
+			scriptURL.searchParams.set( 'onload', onLoadFn );
+			scriptURL.searchParams.set( 'render', 'explicit' );
+
+			this.readyPromise = deferred.promise();
+			window[ onLoadFn ] = deferred.resolve;
+			mw.loader.load( scriptURL.toString() );
+		}
+
+		return this.readyPromise;
+	};
+
+	ve.init.mw.HCaptchaSaveErrorHandler.static.matchFunction = function ( data ) {
+		const captchaData = ve.getProp( data, 'visualeditoredit', 'edit', 'captcha' );
+
+		return !!( captchaData && captchaData.type === 'hcaptcha' );
+	};
+
+	ve.init.mw.HCaptchaSaveErrorHandler.static.process = function ( data, target ) {
+		const self = this,
+			siteKey = config.HCaptchaSiteKey,
+			$container = $( '<div>' );
+
+		// Register extra fields
+		target.saveFields.wpCaptchaWord = function () {
+			// eslint-disable-next-line no-jquery/no-global-selector
+			return $( '[name=h-captcha-response]' ).val();
+		};
+
+		this.getReadyPromise()
+			.then( () => {
+				// ProcessDialog's error system isn't great for this yet.
+				target.saveDialog.clearMessage( 'api-save-error' );
+				target.saveDialog.showMessage( 'api-save-error', $container, { wrap: false } );
+				self.widgetId = window.hcaptcha.render( $container[ 0 ], {
+					sitekey: siteKey,
+					callback: function () {
+						target.saveDialog.executeAction( 'save' );
+					},
+					'expired-callback': function () {},
+					'error-callback': function () {}
+				} );
+				target.saveDialog.popPending();
+				target.saveDialog.updateSize();
+
+				target.emit( 'saveErrorCaptcha' );
+			} );
+	};
+
+	ve.init.mw.saveErrorHandlerFactory.register( ve.init.mw.HCaptchaSaveErrorHandler );
+};

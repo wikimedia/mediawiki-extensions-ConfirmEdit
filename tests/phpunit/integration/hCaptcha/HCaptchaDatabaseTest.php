2cc3146588ffdf64e4fcf767c4b8caf435ddd6bd
Dreamy Jazz <wpgbrown@wikimedia.org>
2026-05-15 22:23:43 +0100

    HCaptcha: Add test for force show CAPTCHA mid request
    Why:
    * In 2cc45f269c15bd3fff6c7d3fc1f8ca96bf157fb3 we fixed an issue
      where the hCaptcha "force show CAPTCHA" handling would fail
      if the flag was set mid request while the
      'wgConfirmEditForceShowCaptcha' flag was set in the request
    * A recent commit changed the behaviour of this code and
      dropped the test that was verifying this security fix
    ** This commit adds it back using a more end to end approach
       so that we can be sure we do not reintroduce the security issue
    
    What:
    * Add a test to HCaptchaDatabaseTest that checks the
      ::doConfirmEditMerged method will return a CAPTCHA edit failure
      when the force show CAPTCHA has been set mid-request and after
      the first CAPTCHA verification
    
    Bug: T426056
    Change-Id: Ibeefd0701ddbee4ca9138d06ad7e560b2302f18a

--- tests/phpunit/integration/hCaptcha/HCaptchaDatabaseTest.php
+++ tests/phpunit/integration/hCaptcha/HCaptchaDatabaseTest.php
@@ -6,11 +6,14 @@ namespace MediaWiki\Extension\ConfirmEdit\Tests\Integration\hCaptcha;
 
 use MediaWiki\Content\ContentHandler;
 use MediaWiki\Context\RequestContext;
+use MediaWiki\EditPage\EditPage;
 use MediaWiki\Extension\ConfirmEdit\hCaptcha\HCaptcha;
+use MediaWiki\Http\HttpRequestFactory;
 use MediaWiki\Http\MWHttpRequest;
 use MediaWiki\Json\FormatJson;
 use MediaWiki\Request\FauxRequest;
 use MediaWiki\Status\Status;
+use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
 use MockHttpTrait;
 
@@ -55,5 +58,69 @@ class HCaptchaDatabaseTest extends MediaWikiIntegrationTestCase {
 			$context, ContentHandler::makeContent( '', $title ), $status, '', $user, false
 		) );
 		$this->assertStatusError( 'hcaptcha-force-show-captcha-edit', $status );
+	}
+
+	public function testConfirmEditMergedWhenForceCaptchaSetAfterFirstCheck(): void {
+		// Mock that the first CAPTCHA check has a successful siteverify API call (the second
+		// should not reach the siteverify API call)
+		$siteVerifyRequest = $this->createMock( MWHttpRequest::class );
+		$siteVerifyRequest->method( 'execute' )
+			->willReturn( Status::newGood() );
+		$siteVerifyRequest->method( 'getContent' )
+			->willReturn( FormatJson::encode( [
+				'success' => true,
+				'sitekey' => 'normal-key',
+			] ) );
+
+		// The first solve performs siteverify, but the force-show failure path should not.
+		$mockHttpRequestFactory = $this->createMock( HttpRequestFactory::class );
+		$mockHttpRequestFactory->expects( $this->once() )
+			->method( 'create' )
+			->willReturn( $siteVerifyRequest );
+		$this->setService( 'HttpRequestFactory', $mockHttpRequestFactory );
+
+		$hCaptcha = new HCaptcha();
+		$hCaptcha->setConfig( [
+			'HCaptchaAlwaysChallengeSiteKey' => 'challenge-key',
+			'HCaptchaSiteKey' => 'normal-key',
+		] );
+		$hCaptcha->setAction( 'edit' );
+
+		$request = new FauxRequest( [ 'h-captcha-response' => 'abcdef' ], true );
+
+		// Expect that the first "force show captcha" check passes, so that we can see a difference further below
+		$user = $this->getTestUser()->getUser();
+		$this->assertTrue( $hCaptcha->passCaptchaFromRequest( $request, $user ) );
+		$this->assertNull( $hCaptcha->getError() );
+		$this->assertTrue( $hCaptcha->isCaptchaSolved() );
+
+		$hCaptcha->setForceShowCaptcha( true );
+
+		// Using ::confirmEditMerged to check, make sure that the force show captcha flag now being set
+		// causes hCaptcha to be considered not passed and so causes a CAPTCHA edit failure
+		$context = new RequestContext();
+		$context->setUser( $user );
+		$title = Title::newFromText( 'ForceShowCaptchaTestPage' );
+		$context->setTitle( $title );
+		$context->setRequest( new FauxRequest( [ 'captchaword' => 'abcdef' ], true ) );
+
+		$status = Status::newGood();
+		$this->assertFalse(
+			$hCaptcha->confirmEditMerged(
+				$context,
+				ContentHandler::makeContent( '', $title ),
+				$status,
+				'',
+				$user,
+				false
+			),
+			'Edit should fail when force-show mode is enabled after a normal captcha solve'
+		);
+		$this->assertSame( EditPage::AS_HOOK_ERROR_EXPECTED, $status->value );
+		$this->assertArrayHasKey( 'captcha', $status->statusData );
+		$this->assertSame( 'hcaptcha', $status->statusData['captcha']['type'] );
+		$this->assertSame( 'challenge-key', $status->statusData['captcha']['key'] );
+
+		$this->assertFalse( $hCaptcha->isCaptchaSolved() );
 	}
 }

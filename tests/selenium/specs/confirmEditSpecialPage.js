814a4ea5eceea5b9af1977c066941207df103b71
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2026-06-09 15:47:59 +0000

    Revert "ConfirmEdit: Add Selenium test infrastructure"
    Why:
    * We decided in the end that we did not need Selenium tests
      and so we should revert adding the Selenium test
      infrastructure
    
    What:
    * Revert 7670a36ef9e92c6e12535cb0e04347470939d1a4
    
    Bug: T420720
    Change-Id: Ib2c47a9bb43eca2adbe3f9d88d79e7937de99e87

--- tests/selenium/specs/confirmEditSpecialPage.js
+++
@@ -1,8 +0,0 @@
-import MainPage from '../pageobjects/main.page.js';
-
-describe( 'ConfirmEdit', () => {
-	it( 'Main page should be accessible', async () => {
-		await MainPage.open();
-		await expect( MainPage.heading ).toBeDisplayed();
-	} );
-} );
--- tests/selenium/wdio.conf.js
+++
@@ -1,3 +0,0 @@
-import { config as wdioDefaults } from 'wdio-mediawiki/wdio-defaults.conf.js';
-
-export const config = { ...wdioDefaults };
